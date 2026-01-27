89ebd9b83029927debbbb8b0238683efb9135b94
John Chishugi <jkchishugi@gmail.com>
2026-01-27 13:49:07 +0200

    ft-done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { SettingsProvider } from '@/components/settings-provider';
 import { ClientDbProvider } from '@/components/client-db-provider';
 import { QueryProvider } from '@/components/providers/query-provider';
+import { DataPreloader } from '@/components/providers/data-preloader';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-body antialiased bg-background`}>
         <QueryProvider>
+          <DataPreloader />
           <ClientDbProvider>
             <SettingsProvider>
               <AppShell>{children}</AppShell>
